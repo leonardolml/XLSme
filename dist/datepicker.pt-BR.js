e53a3c76498831a7781fc82102d58db393a5f4f7
leonardolml <leonardolml@gmail.com>
2018-02-22 22:30:29 -0300

    Datepicker

---
+++ dist/datepicker.pt-BR.js
@@ -0,0 +1,11 @@
+// Datepicker.pt-br.js
+$.fn.datepicker.languages['pt-BR'] = {
+    format: "dd/mm/yyyy",
+    days: ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"],
+    daysShort: ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sáb"],
+    daysMin: ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sab"],
+    months: ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"],
+    monthsShort: ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"],
+    weekStart: 1,
+    startView: 0,
+};
